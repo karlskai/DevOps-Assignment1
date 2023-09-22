6021e7b0d571fbf468d637b62ba77c08d556b4de
karlskai <97944515+karlskai@users.noreply.github.com>
2023-09-22 18:58:11 +0300

    simplecalc

---
+++ assignment1/src/assignment1/SimpleCalculator.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        double num1 = 3.0;
+        double num2 = 3.0;
+
+        double result = num1 + num2;
+
+        System.out.println("Result of 3 + 3 is: " + result);
+    }
+}
