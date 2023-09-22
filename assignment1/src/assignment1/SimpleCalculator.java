543c18f0285ace7c1596c2b208619d9215f52b12
karlskai <97944515+karlskai@users.noreply.github.com>
2023-09-22 19:13:01 +0300

    new

--- assignment1/src/assignment1/SimpleCalculator.java
+++ assignment1/src/assignment1/SimpleCalculator.java
@@ -1,12 +1,58 @@
 package assignment1;
+import java.util.Scanner;
 
 public class SimpleCalculator {
-    public static void main(String[] args) {
-        double num1 = 3.0;
-        double num2 = 3.0;
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
-        double result = num1 + num2;
+        while (!exit) {
+            System.out.print("Enter the first number: ");
+            double num1 = scanner.nextDouble();
 
-        System.out.println("Result of 3 + 3 is: " + result);
+            System.out.print("Enter the second number: ");
+            double num2 = scanner.nextDouble();
+
+            System.out.println("Select an operation:");
+            System.out.println("1. Addition (+)");
+            System.out.println("2. Subtraction (-)");
+            System.out.println("3. Multiplication (*)");
+            System.out.println("4. Division (/)");
+            System.out.println("5. Exit");
+
+            int choice = scanner.nextInt();
+            double result = 0;
+
+            switch (choice) {
+                case 1:
+                    result = num1 + num2;
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    break;
+                case 4:
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Error: Division by zero");
+                    }
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+
+            if (!exit) {
+                System.out.println("Result: " + result);
+            }
+        }
+
+        System.out.println("Calculator is exiting. Goodbye!");
+        scanner.close();
     }
 }
